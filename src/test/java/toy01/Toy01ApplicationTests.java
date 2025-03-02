b2cdb940a36f13a64c94dc4e1f9bbd3b622226be
eunsik jung <bamtol2100@naver.com>
2025-03-02 16:34:19 +0900

    초기 설정

---
+++ src/test/java/toy01/Toy01ApplicationTests.java
@@ -0,0 +1,13 @@
+package toy01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Toy01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
